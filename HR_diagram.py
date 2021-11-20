04f65d57baecdb45bb722c9613d1f0bb4fc52420
RayanMalekShkeiru <rayan.malek94@gmail.com>
2021-11-20 17:39:57 +0100

    Updating ADQL request

--- HR_diagram.py
+++ HR_diagram.py
@@ -10,21 +10,30 @@ from sklearn.decomposition import PCA
 
 
 
+
 coord=SkyCoord(ra=280,dec=60, unit=(u.degree, u.degree), frame='icrs')
 width=u.Quantity(0.1, u.deg)
 height=u.Quantity(0.1, u.deg)
-job = Gaia.launch_job_async("SELECT TOP 1000 gaia_source.paralla \
-    x,gaia_source.phot_g_mean_mag,gaia_source.bp_rp \
-        FROM gaiaedr3.gaia_source \
-            WHERE CONTAINS(POINT('ICRS',gaiaedr3.gaia_source.ra,gaiaedr3.gaia_source.dec),CIRCLE('ICRS',0,0,180))=1")
+job = Gaia.launch_job_async("SELECT ra, dec, parallax, phot_g_mean_mag, bp_rp, \
+astrometric_excess_noise, \
+phot_bp_rp_excess_factor \
+FROM gaiaedr3.gaia_source \
+WHERE parallax > 10 \
+AND parallax_over_error > 10 \
+AND phot_bp_mean_flux_over_error > 10 \
+AND phot_rp_mean_flux_over_error > 10")
 print(job)
 r=job.get_results()
 r.pprint()
 #%%
 R= r.to_pandas()
+ext = R["astrometric_excess_noise"].max()-R["astrometric_excess_noise"].min()
 #%%
+import plotly.express as px
 X = R["bp_rp"]
 Y = R["phot_g_mean_mag"] + 5*np.log10(R["parallax"]/100)
-plt.scatter(X, Y)
+fig = px.scatter(x=X, y=Y, color=R["astrometric_excess_noise"],
+                 title="Numeric 'size' values mean continuous color")
 plt.gca().invert_yaxis()
+fig.show()
 # %%
